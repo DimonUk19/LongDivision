8263bedd79664de0d3ddc6d030c9f15a340f8001
Kvasha Dmytro <dalytons6@gmail.com>
2022-12-29 21:03:30 +0200

    First version Division program

---
+++ src/main/java/org/example/View.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class View {
+
+    public static final String INPUT_DIVIDEND = "Input dividend:";
+    public static final String INPUT_DIVIDER = "Input divider:";
+    public static final String WRONG_INPUT = "Input integer!";
+    public static final String NOT_ZERO = "Input a number except zero:";
+    public void printlnMessage(String message) {
+        System.out.println(message);
+    }
+
+}
+
